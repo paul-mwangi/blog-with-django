a4bd87f8b776bd4fc2c65e67d381cdd5e673bdc6
PNINE <johnpaulmwangin@gmail.com>
2019-03-06 17:00:25 +0300

    Initial commit

---
+++ blog/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from blog.models import Post
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Post.objects.create(title='big', text='unique unique', author_id='1')
+
+    def test_title_max_length(self):
+        post = post.objects.get(id=1)
+        max_length = post._meta.get_field('title').max_length
+        self.assertEquals(max_length, 200)
