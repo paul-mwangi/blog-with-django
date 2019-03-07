ff676e46226db7b1416582aa44eeeeb7581ae9b2
PNINE <johnpaulmwangin@gmail.com>
2019-03-07 09:26:22 +0300

    hihi

--- blog/tests.py
+++ blog/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from blog.models import Post
 
+
 class PostModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        Post.objects.create(title='big', body='unique unique', author_id='1')
+        Post.objects.create(title='Big', body='unique unique', author_id='1')
 
-    def test_title_max_length(self):
+    def test_title_label(self):
         post = Post.objects.get(id=1)
-        max_length = post._meta.get_field('title').max_length
-        self.assertEquals(max_length, 250)
+        field_label = post._meta.get_field('title').verbose_name
+        self.assertEquals(field_label, 'title')
