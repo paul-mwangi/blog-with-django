b3f02b93f6ea718a3f182f191e003f26d612a1be
PNINE <johnpaulmwangin@gmail.com>
2019-03-07 09:03:19 +0300

    lili

--- blog/tests.py
+++ blog/tests.py
@@ -4,9 +4,9 @@ from blog.models import Post
 class PostModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        Post.objects.create(title='big', text='unique unique', author_id='1')
+        Post.objects.create(title='big', body='unique unique', author_id='1')
 
     def test_title_max_length(self):
         post = post.objects.get(id=1)
         max_length = post._meta.get_field('title').max_length
-        self.assertEquals(max_length, 200)
+        self.assertEquals(max_length, 250)
